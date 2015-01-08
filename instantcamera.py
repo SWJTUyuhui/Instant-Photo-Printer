40fbb47e6bace69196bc3d13c823e5e1b4bffa64
Hui Yu <yuhui20080701@gmail.com>
2015-01-08 21:26:15 +0800

    Update instantcamera.py

--- instantcamera.py
+++ instantcamera.py
@@ -2,28 +2,31 @@ import time
 import picamera
 #from datetime import datetime
 from PIL import Image
-import RPi.GPIO 
+import RPi.GPIO
 import io
 import os
 Light = 17
+flag=0
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 RPi.GPIO.setup(Light,RPi.GPIO.IN)
 
 camera = picamera.PiCamera()
 
-def newimage() :       
+def newimage() :
         camera.capture('/home/pi/inst.jpg')
-        print "captured " 
+        print "captured "
 while 1:
 
-	if  RPi.GPIO.input(Light)!=1:    #if the level is high, take  pictures
-		if n==1:
-			os.remove('/home/pi/inst.jpg') 
-		newimage()
-		n=1
-		time.sleep(10)
-		os.system('export PRINTER=pi1010')
-		os.system('lp -d pi1010 inst.jpg')
-		time.sleep(10)
-	
-
+	if  RPi.GPIO.input(Light)!=1:    #if the level is high, take N pictures
+		if flag==0:
+			newimage()
+			time.sleep(10)
+			os.system('export PRINTER=pi1010')
+			os.system('lp -d pi1010 inst.jpg')
+			flag=1
+		else:
+			os.remove('/home/pi/inst.jpg')
+			newimage()
+			time.sleep(10)
+			os.system('lp -d pi1010 inst.jpg')
+			time.sleep(20)
